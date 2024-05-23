61e5ad529094f9f363b3459a1ff867229f0096a5
LostSoul <miro.slezak0601@gmail.com>
2024-05-23 13:27:00 +0200

    improved Score.java

--- src/service/Score.java
+++ src/service/Score.java
@@ -1,20 +1,48 @@
 package service;
 
-// So far it's just a dummy class. I will improve upon it later
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Score {
+    private static int totalScore = 0;
+    private static int streakCount = 0;
+    private static int multiplier = 1;
+
     public static void PerfectHit() {
-        System.out.println("Perfect hit!");
+        totalScore += 100 * multiplier;
+        streakCount++;
+        updateMultiplier();
+        System.out.println("Perfect hit! Score: " + totalScore);
     }
 
     public static void GoodHit() {
-        System.out.println("Good hit!");
+        totalScore += 50 * multiplier;
+        streakCount++;
+        updateMultiplier();
+        System.out.println("Good hit! Score: " + totalScore);
     }
 
     public static void BadHit() {
-        System.out.println("Bad hit!");
+        totalScore += 25 * multiplier;
+        streakCount = 0;
+        updateMultiplier();
+        System.out.println("Bad hit! Score: " + totalScore);
     }
 
     public static void Miss() {
-        System.out.println("Missed hit!");
+        streakCount = 0;
+        updateMultiplier();
+        System.out.println("Missed hit! Score: " + totalScore);
+    }
+
+    private static void updateMultiplier() {
+        multiplier = Math.min(1 + streakCount / 4, 4); // Maximum multiplier is 4x
+    }
+
+    public static int getTotalScore() {
+        return totalScore;
+    }
+
+    public static int getMultiplier() {
+        return multiplier;
     }
 }
